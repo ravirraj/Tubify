6f75d5e091632bbe3ba3b98a3918f17d2394cc2d
raviraj <ravirrajpatil@gmail.com>
2025-07-20 13:01:49 +0530

    custom api responce and erros

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB CONNECTION ERROR", err);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asynsHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asynsHandler };
