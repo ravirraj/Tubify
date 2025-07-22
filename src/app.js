46c2b535d975107494cca5a5d2b1130063c18446
raviraj <ravirrajpatil@gmail.com>
2025-07-22 21:27:56 +0530

    configration done

--- src/app.js
+++ src/app.js
@@ -15,4 +15,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes
+
+import userRouter from "./routes/user.route.js";
+
+//routes delecrations
+app.use("/api/v1/users", userRouter);
+
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
-const asynsHandler = (requestHandler) => {
-  (req, res, next) => {
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
 
-export { asynsHandler };
+export { asyncHandler };
